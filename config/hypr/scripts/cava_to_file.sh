67372c20e7dbca39a5998dfd589abe317a4e98e2
Ch0p5h0p <samjpoyner@icloud.com>
2026-08-30 23:19:28 -0400

    remove hypr and waybar, move wallpapers into noctalia, clean up noctalia

--- config/hypr/scripts/cava_to_file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# ~/.config/hypr/scripts/cava_to_file.sh
-
-RAMFILE="/dev/shm/cava_output.txt"
-
-cava -p /home/radar77/.config/cava/config_hyprlock | while read -r line; do
-    echo "$line" > "$RAMFILE"
-done
